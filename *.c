0bd8bcd313ddb973bd48f0894b6583f1982ccfa9
Ongii7 <ongii.k18@gmail.com>
2022-10-21 16:59:02 -0700

    initial push with outline

---
+++ *.c
@@ -0,0 +1,18 @@
+//search-multiple-function branch
+/**
+ * In EmployeeOne.c,
+ * create search function for phone entry
+ * compare strings to check if they are equal, essentially same functions as before.
+ * create search function for salary entry
+ * check if the number is equal to an employee's salary.
+ * show example of match & no match in each function.
+ * update employeeMain.c due to 4 cases.
+ **/
+
+//single-search-pass-function branch
+/**
+ * In EmployeeTwo.c
+ * create wrapper function for phone entry and return searchEmployeeTable
+ * create wrapper function for salary entry and return searchEmployeeTable
+ * employeeMain.c should be the same.
+ **/
